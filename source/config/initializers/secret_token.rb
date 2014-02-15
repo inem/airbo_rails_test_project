e2da722b7a411030dc483a384d186f985d63e2ce
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-02-15 14:52:47 +0700

    rails app + gems + spec_helper

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airbo::Application.config.secret_key_base = '9b8cf43b0b908e651a109c78e9606c7efe2c32666bb813caafb653fe9c0b11f431f996d816bcb33908dce70703ee36e9d928817487fe913180760477679b72d4'
